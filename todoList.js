0f76669bbfd5e71e45e9393d70468a63d9815a14
Amir Badrudeen <amirbadrudeen@gmail.com>
2024-08-13 14:20:32 -0400

    Made page more responsive, fixed size of To-Do List Table!

--- todoList.js
+++ todoList.js
@@ -33,11 +33,13 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Oops! It looks like you forgot to write down your task. Even superheroes need a plan!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
+  // Add classes to the new element
+  li.classList.add("w-50", "mx-auto");
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
